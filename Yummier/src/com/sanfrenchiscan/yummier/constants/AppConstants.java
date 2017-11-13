175c5604dd8d5b175b25a0149c5cc3aea01a0482
Cedric Lignier <cedric.lignier@free.fr>
2017-11-12 16:25:49 -0800

    - Change behavior of TodayMenuActivity to always pull the next available menu (there was no menu when the day of the week was either Sat/Sun) - Add Google Custom Search API Key - Add Constants to support new cafe and update code accordingly

--- Yummier/src/com/sanfrenchiscan/yummier/constants/AppConstants.java
+++ Yummier/src/com/sanfrenchiscan/yummier/constants/AppConstants.java
@@ -31,14 +31,21 @@ public class AppConstants {
 	public static final String BONAPPETIT_CAFE_YAHOO_URL = "684";
 	public static final String BONAPPETIT_CAFE_YAHOO_BUILDING_E = "682";
 	public static final String BONAPPETIT_CAFE_YAHOO_BUILDING_F = "683";
-	public static final String BONAPPETIT_CAFE_YAHOO_BUILDING_G = "674";
-
+	public static final String BONAPPETIT_CAFE_YAHOO_BUILDING_G = "674"; // CLOSED SINCE 2016
+	public static final String BONAPPETIT_CAFE_YAHOO_URL_JUICE_BAR = "674"; // NO MENU
+	public static final String BONAPPETIT_CAFE_YAHOO_SF_MISSION_STREET = "885"; 
+	public static final String BONAPPETIT_CAFE_YAHOO_VERVE_COFFEE_BAR = "674"; // NO MENU
+	
 	public static final String BONAPPETIT_CORICONS_VEGETARIAN = "vegetarian";
 	public static final String BONAPPETIT_CORICONS_VEGAN = "vegan";	   
 	public static final String BONAPPETIT_CORICONS_GLUTENFREE = "gluten free";	   
 	public static final String BONAPPETIT_CORICONS_ORGANIC = "organic";	
 	public static final String BONAPPETIT_CORICONS_SEAFOODWATCH = "seafood watch";	
 	public static final String BONAPPETIT_CORICONS_FARMTOFORK = "farm to fork";	
+	
+	//Image Search API Info
+	public static final String GOOGLESEARCH_APIKEY_YUMMIER = "AIzaSyAURQ69O_ceYzLQsyEbu0LwBcdBGIXpuXo";
+	public static final String GOOGLESEARCH_CS_YUMMIER = "008757292700228420593:az7dv48gy4a";
 	
 }
 
